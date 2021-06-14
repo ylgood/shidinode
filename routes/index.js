ef99833522bfbb7b75d820086f11108e2e6c5ca4
yanglei <yanglei.666@bytedance.com>
2021-06-14 23:03:08 +0800

    1

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = require('koa-router')();
 router.get('/*', async (ctx, next) => {
   await ctx.render('index', {
       basic: 'https://ylgood.github.io/',
-      js: 'e9639486.js',
+      js: 'ff690bdc.js',
       css: '9372b12a.css',
   })
 });
